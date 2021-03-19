71ec9a4988a01a2b048e8b7f1581c53a76bdb859
Piotr <piotrblonski96@gmail.com>
2021-03-19 13:32:43 +0100

    ndcg

--- src/util/metrics.py
+++ src/util/metrics.py
@@ -3,15 +3,15 @@ from typing import List, Tuple
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
+from sklearn.metrics import ndcg_score
 
 from src.models.recommender import Recommender
 
 
 def mean_reciprocal_rank(
-    test_discretized_ratings: pd.DataFrame,
-    model: Recommender
+        test_discretized_ratings: pd.DataFrame,
+        model: Recommender
 ) -> Tuple[float, List[float]]:
-
     ranks = []
 
     test_discretized_ratings = test_discretized_ratings.groupby("userId")
@@ -33,7 +33,36 @@ def mean_reciprocal_rank(
 
 
 def mean_average_precision(
-    test_discretized_ratings: pd.DataFrame,
-    model: Recommender
+        test_discretized_ratings: pd.DataFrame,
+        model: Recommender
 ) -> Tuple[float, List[float]]:
     pass
+
+
+def mean_ndcg(
+        test_discretized_ratings: pd.DataFrame,
+        model: Recommender
+) -> Tuple[float, List[float]]:
+    ranks = []
+
+    test_discretized_ratings = test_discretized_ratings.groupby("userId")
+    iterator = tqdm(test_discretized_ratings, desc="Testing predictions")
+
+    for user_id, user_ratings in iterator:
+        pred_movies_score = model.predict_scores(user_id)
+
+        pred_movies = pred_movies_score[0]
+        pred_score = pred_movies_score[1]
+
+        y_true = np.asarray([user_ratings['liked'].values])
+        y_idx = user_ratings['movieId'].values
+
+        indices = [pred_movies.index(movie_id) for movie_id in y_idx]
+
+        pred_movies = np.take(pred_movies, indices)
+        pred_score = np.asarray([np.take(pred_score, indices)])
+        if pred_score.size > 1:  # If there is no enough data -then ignore score counting for that user
+            ndcg = ndcg_score(y_true, pred_score)
+            ranks.append(ndcg)
+
+    return np.mean(ranks), ranks
--- src/util/test/test_metrics.py
+++ src/util/test/test_metrics.py
@@ -30,3 +30,15 @@ def test_mean_average_precision(
     assert isinstance(ranks, list)
     assert all(isinstance(rank, float) for rank in ranks)
     assert ranks == [0.5, 1.0, 0.0]  # ! TODO: fix later
+
+def test_mean_ngdc(
+        test_discretized_ratings: pd.DataFrame,
+        model: Recommender
+):
+    mean, ranks = metrics.mean_ndcg(test_discretized_ratings,model)
+    assert isinstance(mean, float)
+    assert isinstance(ranks, list)
+    assert ranks  # check if ranks is not empty list
+    assert all(isinstance(rank, float) for rank in ranks)
+    
+    pass
