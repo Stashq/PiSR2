d6f646e18164c340db5348c4c74cc61be6b29764
Stanisław Straburzyński <sstraburzynski@gmail.com>
2021-03-20 21:43:29 +0100

    Mean Average Precision and test defined

--- src/util/metrics.py
+++ src/util/metrics.py
@@ -34,6 +34,74 @@ def mean_reciprocal_rank(
 
 def mean_average_precision(
     test_discretized_ratings: pd.DataFrame,
-    model: Recommender
+    model: Recommender,
+    N: int
 ) -> Tuple[float, List[float]]:
-    pass
+    """Calculate mean average precision.
+
+    Parameters
+    ----------
+    test_discretized_ratings : pd.DataFrame
+        Dataframe containing true users films ratings.
+    model : Recommender
+        Tested model.
+    N : int
+        Number of movies taken into account in recommendation for single user.
+
+    Returns
+    -------
+    Tuple[float, List[float]]
+        1. Mean average precision averaged for all users.
+        2. List of average precision for all users separately.
+    """
+    ranks = []
+    test_discretized_ratings = test_discretized_ratings.groupby("userId")
+    iterator = tqdm(test_discretized_ratings, desc="Testing predictions")
+    for user_id, user_ratings in iterator:
+        pred_movies = model.predict(user_id)[:N]
+        N_user_favourites = user_ratings[user_ratings["liked"]]
+        N_user_favourites = N_user_favourites.sort_values(by="rating", ascending=False)
+        N_user_favourites = N_user_favourites["movieId"].values
+
+        user_score = get_user_AP_score(pred_movies, N_user_favourites, N)
+        ranks.append(user_score)
+        
+    return np.mean(ranks), ranks
+
+def get_user_AP_score(
+    pred_movies: List[int],
+    N_user_favourites: List[int],
+    N: int
+) -> float:
+    """Calculate average precision for user.
+
+    Parameters
+    ----------
+    pred_movies : List[int]
+        Sorted (descending) list of movies predicted by model for user to recommend.
+    N_user_favourites : List[int]
+        Most favourite user N movies.
+    N : int
+        Number of movies taken into account in recommendation for single user.
+
+    Returns
+    -------
+    float
+        Average precision for user recommendation.
+    """
+    if N > len(pred_movies):
+        N = len(pred_movies)
+    sublist_scores = []
+    for sublist_len in range(1, N+1):
+        n_correct = 0
+        for i, movie_id in enumerate(reversed(pred_movies[:sublist_len])):
+            if i == 0 and movie_id not in N_user_favourites:
+                break
+            if movie_id in N_user_favourites:
+                n_correct += 1
+        if n_correct != 0:
+            sublist_scores.append(n_correct/sublist_len)
+    if len(sublist_scores) == 0:
+        return 0.0
+    else:
+        return np.mean(sublist_scores)
