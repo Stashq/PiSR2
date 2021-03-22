87e20cef854332500590e138586df6a02c598627
Piotr <piotrblonski96@gmail.com>
2021-03-23 00:14:47 +0100

    Model prototype with helping functions

---
+++ src/models/contentBased/data.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from sklearn import preprocessing
+from torch.utils.data import TensorDataset
+
+from src.util.data import get_train_test_ratings
+
+column_list = [
+    "id",
+    "adult",
+    "budget",
+    "popularity",
+    "runtime",
+    "vote_average",
+    "vote_count",
+    "original_language",
+    "belongs_to_collection",
+    "spoken_languages",
+    "genres",
+]
+import pickle
+
+import torch
+
+EMBEDDING_PATH = "../emb.pkl"
+
+
+def get_dataset_eval(self, Embeddings=False) -> pd.DataFrame:
+    movies = self.movies
+    ratings = self.ratings
+    to_drop = ["1997-08-20", "2012-09-29", "2014-01-01"]
+    for drop_error in to_drop:
+        movies = movies[movies.id != drop_error]
+    movies.id = movies.id.astype("int64")
+    dataset = ratings.merge(movies[column_list], left_on="movieId", right_on="id")
+
+    dataset = dataset.dropna()
+
+    budget_scaler = preprocessing.StandardScaler().fit(
+        dataset.budget.to_numpy().reshape(-1, 1)
+    )
+
+    lang_encoder = preprocessing.LabelEncoder().fit(dataset.original_language)
+
+    genres_encoder = preprocessing.LabelEncoder().fit(dataset.genres)
+
+    spoken_languages_encoder = preprocessing.LabelEncoder().fit(
+        dataset.spoken_languages
+    )
+
+    adult_encoder = preprocessing.LabelEncoder().fit(dataset.adult)
+
+    belongs_to_collection_encoder = preprocessing.LabelEncoder().fit(
+        dataset.belongs_to_collection
+    )
+
+    dataset.belongs_to_collection = belongs_to_collection_encoder.transform(
+        dataset.belongs_to_collection
+    )
+    dataset.original_language = lang_encoder.transform(dataset.original_language)
+    dataset.adult = adult_encoder.transform(dataset.adult)
+    dataset.spoken_languages = spoken_languages_encoder.transform(
+        dataset.spoken_languages
+    )
+    dataset.genres = genres_encoder.transform(dataset.genres)
+    dataset.budget = budget_scaler.transform(dataset.budget.to_numpy().reshape(-1, 1))
+    dataset.popularity = dataset.popularity.astype("float64")
+
+    dataset.userId = dataset.userId.astype(int)
+    dataset.movieId = dataset.movieId.astype(int)
+
+    if Embeddings:
+        movies_emb = pickle.load(open(EMBEDDING_PATH, "rb"))
+        dataset = dataset.merge(
+            movies_emb[["id", "vector"]], left_on="movieId", right_on="id"
+        )
+        dataset = dataset.drop(["id_x", "id_y"], axis=1)
+    else:
+        dataset = dataset.drop(["id"], axis=1)
+    dataset = dataset.drop(["timestamp"], axis=1)
+    return dataset
+
+
+def get_dataset():
+    dataset = get_dataset_eval(True)
+    train_ratings, test_ratings = get_train_test_ratings(dataset)
+    target_train = torch.Tensor(train_ratings.rating.values)
+    target_test = torch.Tensor(test_ratings.rating.values)
+    train_ratings = train_ratings.drop(["movieId", "rating"], axis=1)
+    test_ratings = test_ratings.drop(["movieId", "rating"], axis=1)
+
+    train_ratings_t = torch.Tensor(train_ratings.drop(["vector"], axis=1).values)
+    test_ratings_t = torch.Tensor(test_ratings.drop(["vector"], axis=1).values)
+
+    vec_train = torch.Tensor(list(train_ratings.vector.values))
+    vec_test = torch.Tensor(list(test_ratings.vector.values))
+
+    train = torch.cat((vec_train, train_ratings_t), dim=1)
+    test = torch.cat((vec_test, test_ratings_t), dim=1)
+
+    dataset_tensor_train = TensorDataset(train, target_train)
+    dataset_tensor_test = TensorDataset(test, target_test)
+
+    return dataset_tensor_train, dataset_tensor_test
---
+++ src/models/contentBased/model.py
@@ -0,0 +1,127 @@
+import pickle
+from abc import ABC
+from typing import List, Tuple
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
+
+from src.models.contentBased.data import get_dataset_eval
+from src.models.recommender import Recommender
+
+
+class ContentBaseRecommenderSystem(nn.Module, Recommender):
+    def __init__(self, input_size, hidden_feature_size):
+        super().__init__()
+        self.movies = get_dataset_eval(Embeddings=True)
+        self.fc1 = nn.Linear(input_size, hidden_feature_size)  # fully connected
+
+        self.fc2 = nn.Linear(hidden_feature_size, hidden_feature_size)
+        self.fc3 = nn.Linear(hidden_feature_size, hidden_feature_size)
+        self.out_layer = nn.Linear(hidden_feature_size, 1)
+        self.act_fn = F.relu
+        self.setup()
+
+    def setup(self):
+        pass
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act_fn(x)
+
+        x = self.fc2(x)
+        x = self.act_fn(x)
+
+        x = self.act_fn(self.fc3(x))
+        out = F.sigmoid(self.out_layer(x))
+
+        return out * 5
+
+    def predict(self, user_id: int) -> List[int]:
+        """
+        Predicts ranking of movies to watch for a user.
+
+        Parameters
+        ----------
+        user_id : int
+            User's id from the data set.
+
+        Returns
+        -------
+        List[int]
+            List of movies ids. Best recommendations first.
+        """
+        movies, rating = self.predict_scores(user_id)
+        return list(movies)
+
+    def predict_score(self, user_id: int, movie_id: int) -> float:
+        """
+        Predicts score for a given movie that a user would give.
+
+        Parameters
+        ----------
+        user_id : int
+            User's id from the data set.
+        movie_id : int
+            Movie's id from the data set.
+
+        Returns
+        -------
+        float
+            Predicted movie's score in range [0, 5].
+        """
+        df = self.movies.copy()
+
+        df = df.loc[df["movieId"] == movie_id]
+        df_features = df.drop(["movieId", "rating", "vector"], axis=1).loc[0]
+        df_vector = df.vector.loc[0]
+        x_input = torch.Tensor(df_features.values)
+        vec_input = torch.Tensor(list(df_vector))
+
+        input = torch.cat((x_input, vec_input), dim=0)
+
+        prediction = self.forward(input).item()
+        return prediction
+
+    def predict_scores(self, user_id: int) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Predicts scores for all the movies, that a user would give.
+
+        Parameters
+        ----------
+        user_id : int
+            User's id from the data set.
+
+        Returns
+        -------
+        Tuple[np.ndarray, np.ndarray]:
+            Ranked movies with their scores.
+        """
+        df = self.movies.copy()
+        df.userId = user_id
+
+        df = df.drop_duplicates("movieId")
+        movieIds = df.movieId.values
+        df_features = df.drop(["movieId", "rating", "vector"], axis=1)
+        df_vector = df.vector.values
+        x_input = torch.Tensor(df_features.values)
+        vec_input = torch.Tensor(list(df_vector))
+        input = torch.cat((x_input, vec_input), dim=1)
+
+        prediction = self.forward(input).detach().numpy()
+
+        ranking = pd.DataFrame(zip(movieIds, prediction), columns=["movie", "rating"])
+
+        ranking = ranking.sort_values(by="rating", ascending=False)
+        return ranking.movie.values, np.concatenate(ranking.rating.values, axis=0)
+
+    def get_user_data(self, user_id: int, exlude=False) -> pd.DataFrame:
+        df = self.movies.copy()
+        if exlude:
+            df = df.loc[df["userId"] != user_id]
+        else:
+            df = df.loc[df["userId"] == user_id]
+        return df
