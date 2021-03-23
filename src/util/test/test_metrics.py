d2e2d79ff1400eca9b2e767b87d0ed54c7c6b05f
ojqbo <sebastian.jamrozinski@gmail.com>
2021-03-23 12:14:03 +0100

    RMSE metric test: added value based assertion

--- src/util/test/test_metrics.py
+++ src/util/test/test_metrics.py
@@ -50,3 +50,4 @@ def test_coverage(test_discretized_ratings: pd.DataFrame, model: Recommender):
 def test_rmse(test_ratings: pd.DataFrame, model: Recommender):
     rmse = metrics.rmse(test_ratings, model)
     assert isinstance(rmse, float)
+    assert rmse == 0.3878358759406698
